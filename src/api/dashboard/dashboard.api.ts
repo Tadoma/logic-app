e5438ed9a893c885f46c8cd69cfdf82fa2089723
Artem Kozlov <midinq@gmail.com>
2025-09-22 18:27:05 +0400

    Little refactor

--- src/api/dashboard/dashboard.api.ts
+++ src/api/dashboard/dashboard.api.ts
@@ -1,10 +1,6 @@
 import { Course } from './dashboard.models.ts';
 
 export const getCourses: () => Promise<Course[]> = async () => {
-    try {
-        const response = await fetch('https://logiclike.com/docs/courses.json');
-        return await response.json();
-    } catch (error) {
-        console.error(error);
-    }
+    const response = await fetch('https://logiclike.com/docs/courses.json');
+    return await response.json();
 };
